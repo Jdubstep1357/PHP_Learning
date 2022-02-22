f8686c8202df77646ab3feee76aa73dc46b84761
John Lyons <jwlyons2015@gmail.com>
2022-02-22 17:15:33 -0600

    Created update records form

---
+++ mysql/db.php
@@ -0,0 +1,9 @@
+<?php 
+/* THIS CONNECTS TO THE DATABASE */
+
+$connection = mysqli_connect('localhost', 'root', 'root', 'loginapp');
+if($connection) {
+    die("Database connection failed");
+}
+
+?>
