8a8534402484918a7d12ea2208337623684f3f07
John Lyons <jwlyons2015@gmail.com>
2022-03-01 18:14:19 -0600

    Error with Quiz 7 in code

--- Quiz/7.php
+++ Quiz/7.php
@@ -2,6 +2,19 @@
 <?php include "includes/header.php" ?>
     
 
+<?php 
+	/*  Step 1 - Create a database in PHPmyadmin */
+
+
+
+	$connection = mysqli_connect('localhost', 'root', 'root', 'Quiz_Time');
+	if(!$connection) {
+		die("Database connection failed") . mysqli_error($connection);
+	} else {
+		echo "Database connected!"
+	}
+?>
+
 	<section class="content">
 
 		<aside class="col-xs-4">
@@ -18,17 +31,73 @@
 	
 	<?php  
 
-	/*  Step 1 - Create a database in PHPmyadmin
 
-		Step 2 - Create a table like the one from the lecture
 
-		Step 3 - Insert some Data
 
-		Step 4 - Connect to Database and read data
 
-*/
+
+		// Step 2 - Create a table like the one from the lecture
+
+
+
+		// Step 3 - Insert some Data
+
+			if(isset($_POST['submit'])) {
+				global $connection;
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+		
+				$query = "INSERT INTO users(username,password) ";
+				$query .= "VALUES ('$username', '$password')";
+		
+				$result = mysqli_query($connection, $query);
+		
+				if(!$result) {
+					die("Query failed!");
+				} else {
+					echo "Record created!";
+				}
+			}
+
+			$query = "SELECT * FROM users";
+			$result = mysqli_query($connection, $query);
+		
+			if(!$result) {
+				die('Query failed');
+			}
+					// this gathers all the data from rows from php_myadmin
+					while($row = mysqli_fetch_assoc($result)) {
+						print_r($row);
+					 
+					}
+?>
+
+<html>
+	<body>
+		<div class="container">
+			<div class="col-sm-6">
+			<h1 class="text-center">Create</h1>
+				<form action="login_create.php" method="post">
+					<div class="form-group">
+					<label for="username">Username</label>
+						<input type="text" name="username" class="form-control">
+					</div>
+					<div class="form-group">
+					<label for="password">Password</label>
+						<input type="text" name="password" class="form-control">
+					</div>
+					
+					<input class="btn btn-primary" type="submit"  name="submit" value="CREATE">
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
 	
-	?>
+	
 
 
 
