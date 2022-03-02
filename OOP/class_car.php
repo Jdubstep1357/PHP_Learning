fae48211da97833c3bcad3f0366b830aa8c2eb42
John Lyons <jwlyons2015@gmail.com>
2022-03-02 14:59:39 -0600

    Class checking

---
+++ OOP/class_car.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Car {
+
+}
+
+if(class_exists("Car")) {
+    echo "It works!";
+} else {
+    echo "no";
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
