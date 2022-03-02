1518805160365d0f93945f9be00e85abd54c919c
John Lyons <jwlyons2015@gmail.com>
2022-03-02 16:00:59 -0600

    constructor

---
+++ OOP/class_constructor.php
@@ -0,0 +1,38 @@
+<?php 
+
+class Car {
+
+    var $wheel = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+
+    // __ enables constructor
+    function __construct() {
+        echo $this->wheels = 10;
+        }
+
+}
+
+$bmw = new Car();
+
+$truck = new Car();
+
+$semi = new Car();
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
