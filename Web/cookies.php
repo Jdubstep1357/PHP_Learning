ddf3d124e50c71009af32cd2c92b3590f5f9e5ce
John Lyons <jwlyons2015@gmail.com>
2022-03-01 21:25:55 -0600

    Made cookie

---
+++ Web/cookies.php
@@ -0,0 +1,25 @@
+<?php
+
+// 3 parameters: 1) name 2) value 3)
+setcookie('', '', '');
+
+$name = "Somename";
+$value = 100;
+// multiple seconds (60) * minutes (60) * hours (24) * days (7)
+$expiration = time($name,$value,$expiration) + (60*60*24*7);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
